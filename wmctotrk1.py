d43ebc3228686ea8a3442e0b111cd13704d0b820
davhunt <davhunt@indiana.edu>
2018-10-15 21:08:39 -0400

    tidied up outpt directories

--- wmctotrk1.py
+++ wmctotrk1.py
@@ -3,6 +3,7 @@
 import numpy
 import nibabel
 from scipy.io import loadmat
+import os
 
 ident = numpy.eye(4) #TODO allow this to be a variable inputted by user
 
@@ -11,11 +12,15 @@ x = loadmat('/Users/davidhunt/Documents/5bb62f5112512a003f32ed4e/output.mat') #T
 fg_classified = x['fg_classified']
 fg_classified = loadmat('/Users/davidhunt/Documents/5bb62f5112512a003f32ed4e/output.mat')['fg_classified']
 
+os.mkdir("output")
+os.chdir("output") #creates an output directory for the .trk files
 
 for i in range(0, len(fg_classified[0])):  #for each fiber group
 
-    g = fg_classified[0,i][8][0:len(fg_classified[0,i][8])]   #collects all tracts in the group
 
+    g = fg_classified[0,i][8][0:len(fg_classified[0,i][8])]   #collects all tracts in the group
+    os.mkdir(fg_classified[0,i][0][0]) #creates a new folder for each fiber group
+    os.chdir(fg_classified[0,i][0][0])
     for j in range(0, len(g)):  #for each tract in the group
 
         l = []
@@ -28,4 +33,5 @@ for i in range(0, len(fg_classified[0])):  #for each fiber group
         t = nibabel.streamlines.tractogram.Tractogram(streamlines=s, affine_to_rasmm=ident)
         trk = nibabel.streamlines.trk.TrkFile(t)
         trk.save("output" + str(i) + "by" + str(j) + ".trk") #creates the trk file with "i" and "j" (fiber group #, and tract #) appended
-                                #maybe save each fiber group into a different folder?
+
+    os.chdir("..") #moves back to output directory for the next fiber group
