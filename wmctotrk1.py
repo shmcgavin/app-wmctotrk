89abcca96202535917bf3753f8941b942e0a604e
David <davhunt@indiana.edu>
2018-11-29 13:41:43 -0500

    ordered streamlines

--- wmctotrk1.py
+++ wmctotrk1.py
@@ -36,7 +36,7 @@ def save_trk(streamlines, out_file, affine=None, vox_sizes=None, vox_order='LAS'
     hdr['property_name'] = 'track_number'
     #hdr['id_string'] = 'TRACK' + 
     dps = {'track_number': np.array([])}
-    for i in range(0, len(s)):
+    for i in range(0, len(streamlines)):
         dps['track_number'] = np.append(dps['track_number'], i)
     
 
